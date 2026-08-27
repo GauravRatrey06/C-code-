adf31aa41f996db7805a8b06f9412f3bba029282
Gauravratrey <gauravratrey@gmail.com>
2026-08-27 22:28:46 +0530

    Add files via upload

---
+++ datatypesize.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int inttype;
+    float floattype;
+    double doubletype;
+    char chartype;
+    printf("Size of int:%d \n",sizeof(inttype));
+    printf("Size of Float:%d \n",sizeof(floattype));
+    printf("Size of Double:%d \n",sizeof(doubletype));
+    printf("Size of Char:%d \n",sizeof(chartype));
+    return 0;
+}
